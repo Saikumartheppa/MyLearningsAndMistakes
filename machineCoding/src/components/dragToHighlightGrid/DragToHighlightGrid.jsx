e0e408179b51c686efa1b04d6e63eea5036bc877
Saikumartheppa <saikumartheppa06@gmail.com>
2026-09-24 02:09:18 +0530

    Feat: Tracking down the mouse path and Highlighting Squares

--- machineCoding/src/components/dragToHighlightGrid/DragToHighlightGrid.jsx
+++ machineCoding/src/components/dragToHighlightGrid/DragToHighlightGrid.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import { SquareBox } from "../dragToHighlightGrid";
 import { ROWS as rows, COLUMNS as columns } from "../constants";
 import styles from "./style.module.scss";
 const DragToHighlightGrid = () => {
-  return <div className={styles["drag"]}>
-    {Array.from({ length: rows }).map((_, row) =>
-      Array.from({ length: columns }).map((_, column) => (
-        <SquareBox key={`${row}-${column}`} row={row} column={column}/>
-      )),
-    )}
-  </div>;
+  const [isDragging, setIsDragging] = useState(false);
+  const [visitedCells , setVisitedCells] = useState(new Set());
+  const handleDragging = (row , column) => {
+    setIsDragging(true);
+    const coordinates = `${row}-${column}`;
+    setVisitedCells(new Set([coordinates]));
+  }
+  const handlePointerMove = (row , column) => {
+      setVisitedCells((prev) => {
+       const coordinates = `${row}-${column}`;
+        if(prev.has(coordinates)){
+            return prev;
+        }
+        const set = new Set(prev);
+        set.add(coordinates);
+        return set;
+     })
+  }
+  const handlePointerUp = () => {
+    setIsDragging(false);
+  }
+  return (
+    <div className={styles["drag"]}>
+      {Array.from({ length: rows }).map((_, row) =>
+        Array.from({ length: columns }).map((_, column) => (
+          <SquareBox
+            key={`${row}-${column}`}
+            row={row}
+            column={column}
+            isVisited={visitedCells.has(`${row}-${column}`)}
+            isDragging={isDragging}
+            handleDragging={handleDragging}
+            handlePointerMove={handlePointerMove}
+            handlePointerUp={handlePointerUp}
+          />
+        )),
+      )}
+    </div>
+  );
 };
 export default DragToHighlightGrid;
--- machineCoding/src/components/dragToHighlightGrid/SquareBox.jsx
+++ machineCoding/src/components/dragToHighlightGrid/SquareBox.jsx
@@ -1,5 +1,12 @@
 import styles from "./style.module.scss";
-const SquareBox = () => {
-    return <div className={styles["drag__square-box"]}></div>
-}
+const SquareBox = ({ row, column,isVisited, isDragging ,  handleDragging, handlePointerMove , handlePointerUp}) => {
+  return (
+    <div
+      className={`${styles["drag__square-box"]} ${isVisited ? styles["drag--square-bg"] : "" }`}
+      onPointerDown={() => handleDragging(row, column)}
+      onPointerMove={() => isDragging && handlePointerMove(row , column)}
+      onPointerUp={handlePointerUp}
+    ></div>
+  );
+};
 export default SquareBox;
